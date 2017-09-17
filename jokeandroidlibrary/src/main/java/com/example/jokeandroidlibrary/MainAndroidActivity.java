aca3a3ab880edfb700705a4dc48e6988ca50a8e1
Grace Park <gehpark@gmail.com>
2017-09-16 23:58:02 -0700

    Step 2: add android library

---
+++ jokeandroidlibrary/src/main/java/com/example/jokeandroidlibrary/MainAndroidActivity.java
@@ -0,0 +1,25 @@
+package com.example.jokeandroidlibrary;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by gracepark on 9/16/17.
+ */
+
+public class MainAndroidActivity extends AppCompatActivity {
+
+    public final static String EXTRA_JOKE = "extra_joke";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_main);
+
+        Intent intent = getIntent();
+        ((TextView) findViewById(R.id.joke_text)).setText(intent.getStringExtra(EXTRA_JOKE));
+    }
+}
